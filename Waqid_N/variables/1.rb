8b7b9a537ae54e30782b4497d1e3c5aed2256ce7
WaqidN <sandithy.wacitz@gmail.com>
2015-06-12 21:41:26 +0700

    belajar classes dengan variable

---
+++ Waqid_N/variables/1.rb
@@ -0,0 +1,20 @@
+class Bike
+    def initialize(name, Type, negara)
+      @spda_name=name
+      @spda_type=type
+      @spda_negara=negara
+   end
+   def display_details()
+      puts "Bike name #@spda_negara"
+      puts "Bike Type #@spda_type"
+      puts "Bike negara #@spda_negara"
+    end
+end
+
+# Create Objects
+spda1=Bike.new("Nagasawa", "keirin", "Jepang")
+spda2=Bike.new("Tomassini", "Road Bike", "Italia")
+
+# Call Methods
+spdat1.display_details()
+spda2.display_details()
